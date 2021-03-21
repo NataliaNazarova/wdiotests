9b6eed176844165da1d426f4d856a9ae6368c5f3
NataliaNazarova <natalya.i.nazarova@gmail.com>
2021-03-20 21:26:13 -0700

    second commit

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,6 +2,16 @@ import LoginPage from  '../pageobjects/login.page';
 import ProfilePage from '../pageobjects/profile.page';
 
 describe('Auth', () => {
+
+    beforeEach(() => {
+        LoginPage.open();
+  //      browser.refresh();
+    });
+
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()');
+    });
+
     it('user logs in with valid data', () => {
         LoginPage.open();
         LoginPage.setLogin('nhiciwi5818@ichkoch.com');
@@ -10,6 +20,20 @@ describe('Auth', () => {
         ProfilePage.isOpen();
 
     });
+
+    it('submit button is disabled if login and password are absent', () => {
+//        LoginPage.open();
+        LoginPage.submitButtonIsDisabled();
+    });
+
+    it("fails if invalid data provided", () => {
+        LoginPage.setLogin('example@example.com');
+        LoginPage.setPassword('123456');
+        LoginPage.clickSummitButton();
+        LoginPage.errorToastAppeared();
+    });
+
+
 });
 
 
